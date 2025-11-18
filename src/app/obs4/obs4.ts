a855db2939666a586f92ace7a1f9e98e3d143d19
Chaitanya Patil <chaitanyapatil2@M-CRHF57V5K4.local>
2025-11-18 17:42:18 +0530

    commit4

---
+++ src/app/obs4/obs4.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obs4',
+  imports: [],
+  templateUrl: './obs4.html',
+  styleUrl: './obs4.css',
+})
+export class Obs4 {
+
+}
